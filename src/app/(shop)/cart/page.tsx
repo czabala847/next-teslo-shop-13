51cb4345b699d71686453476e2759be1371489bf
Carlos Zabala <czabala847@gmail.com>
2024-02-20 16:08:20 -0500

    carrito de compra vacio

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -11,6 +11,8 @@ const productsInCart = [
 ];
 
 const CartPage = () => {
+  // redirect("/empty");
+
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
